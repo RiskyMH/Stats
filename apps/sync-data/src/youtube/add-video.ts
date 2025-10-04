1a0463b5a93c7967583eab299fa4755830ab662d
RiskyMH <git@riskymh.dev>
2025-10-04 16:30:24 +1000

    maybe? i dont like this solution but it hopefully *will* work

--- apps/sync-data/src/youtube/add-video.ts
+++ apps/sync-data/src/youtube/add-video.ts
@@ -4,6 +4,10 @@ import db from "../drizzle.js";
 import schema from "@stats-compare/db"
 import { sql } from "drizzle-orm/sql"
 
+import { addChannels } from "./add-channel.js";
+import { inArray } from "drizzle-orm";
+import { getChannels } from "./youtube-api.js";
+
 export function addVideos(videos: youtube_v3.Schema$Video[]) {
     const data = [];
     for (const video of videos) {
@@ -25,7 +29,7 @@ export function addVideos(videos: youtube_v3.Schema$Video[]) {
         })
     }
 
-    return db.insert(schema.YouTubeVideo)
+    const insert = () => db.insert(schema.YouTubeVideo)
         .values(data)
         .onConflictDoUpdate({
             target: schema.YouTubeVideo.videoId,
@@ -42,4 +46,19 @@ export function addVideos(videos: youtube_v3.Schema$Video[]) {
             }
         })
         .execute();
+
+        return insert().catch(async e => {
+            const channelIds = videos.map(v => v.snippet?.channelId || '');
+            const channels = await db.query.YouTubeChannel.findMany({
+                columns: {
+                    channelId: true,
+                },
+                where: inArray(schema.YouTubeChannel.channelId, channelIds)
+            });
+            const toFetchChannels = channels.map(c => c.channelId).filter(id => !channelIds.includes(id));
+
+            await addChannels(await getChannels(toFetchChannels) || []);
+            return insert();
+          }
+        );
 }
